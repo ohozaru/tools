4ff012635b356b0734f1d7cccaf10be7193eaab3
Roman Nowicki <peengle@gmail.com>
2012-08-12 15:18:30 +0200

    Correct tests after recent changes

--- php/service_layer/tests/entity_collection.php
+++ php/service_layer/tests/entity_collection.php
@@ -3,7 +3,7 @@ require_once('test_bootstrap.php');
 class Test_EntityCollection extends PHPUnit_Framework_TestCase {
 
     public function test_invalid_service_name() {
-        $collection = Service::get('Test')->getTestCollection();
+        $collection = Service::_('Test')->getTestCollection();
         $this->assertInstanceOf('ArrayIterator', $collection);
         $this->assertEquals('1', $collection->id);
         $this->assertEquals('1', $collection->getId());
--- php/service_layer/tests/service.php
+++ php/service_layer/tests/service.php
@@ -5,11 +5,11 @@ class Test_ServiceService extends PHPUnit_Framework_TestCase {
      * @expectedException RuntimeException
      */
     public function test_invalid_service_name() {
-        Service::get('InvalidName');
+        Service::_('InvalidName');
     }
 
     public function test_getting_service() {
-        $s = Service::get('Test');
+        $s = Service::_('Test');
         $this->assertEquals('Service\Test', $s->__get_working_namespace());
     }
 
@@ -18,11 +18,11 @@ class Test_ServiceService extends PHPUnit_Framework_TestCase {
      * @expectedExceptionMessage Method boo dosen't exist in service Service\Test
      */
     public function test_calling_invalid_method() {
-        $respone = Service::get('Test')->boo();
+        $respone = Service::_('Test')->boo();
     }
 
     public function test_service_response() {
-        $s = Service::get('Test');
+        $s = Service::_('Test');
         $response = $s->foo();
         $this->assertEquals('foo', $response);
     }
@@ -31,13 +31,13 @@ class Test_ServiceService extends PHPUnit_Framework_TestCase {
      * @expectedException RuntimeException
      */
     public function test_invalid_service_call() {
-        $this->assertEquals('boo', Service::get('Test.Invalid')->foo());
+        $this->assertEquals('boo', Service::_('Test.Invalid')->foo());
     }
     
     /**
      * @expectedException RuntimeException
      */
     public function test_sub_service_call() {
-        $this->assertEquals('boo', Service::get('Test.Boo')->foo());
+        $this->assertEquals('boo', Service::_('Test.Boo')->foo());
     }
 }
